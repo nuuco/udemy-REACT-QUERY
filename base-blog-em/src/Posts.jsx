3977a1e81b601ebbc7e3157335fca6b541190273
nuuco <nuuco.coding@gmail.com>
2022-09-18 02:53:25 +0900

    feat: 섹션1 완료 - 리액트 쿼리 적용하기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,7 +16,19 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const {data, isLoading, isError, error} = useQuery("posts", fetchPosts, { staleTime: 5000, cacheTime: 60000 });
+  //useQuery(쿼리 키, 쿼리 함수)
+  //1. 쿼리 키 : 쿼리 이름
+  //2. 쿼리 함수 : 이 쿼리에 대한 데이터를 가져오는 함수. 비동기 함수
+  //data에 fetchPosts 함수가 리턴하는 값이 담긴다.
+
+  if(isLoading) return <h3>Loading...</h3>;
+  if(isError) return (
+    <>
+      <h3>Oops, something went wrong.</h3>
+      <p>{error.toString()}</p>
+    </>
+  )
 
   return (
     <>
